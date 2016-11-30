a84c78971bad11b073a725ebdb93f9f519536571
Yuridia Larios <yuriydani@gmail.com>
2016-11-30 15:32:17 +0000

    added a second database from airtable for our curriculum section, created curriculum menu, added 2 categories and corresponding pics: creating opportunities, and corporate in-site visits

---
+++ allCurriculum.js
@@ -0,0 +1,79 @@
+/*global $*/
+var api_key = 'keyQO62FoqRTWrrGK';
+var all_curriculum_records = 'https://api.airtable.com/v0/appbcB4zyHIHZjntM/Artists?api_key=' + api_key;
+
+
+
+// Creating opportunities curriculum function
+function allCreatingOpportunitiesRecords(data)
+{  
+    $(data.records).each(function(index, curriculum)
+                         {
+                           var curriculum_name = curriculum.fields['Name']
+                           var curriculum_picture = curriculum.fields['Picture']
+                           var curriculum_opportunities = curriculum.fields['OpportunitiesType']
+                          
+                           var curriculum_info = ''
+                           if (curriculum_name && curriculum_opportunities ) {
+                                            curriculum_info += `<div class="column">`;
+                                                curriculum_info += `<div class="col-sm-6 col-md-4">`;
+                                                if (curriculum_picture) {
+                                                    curriculum_info += `<div class="thumbnail">`;
+                                                $.each(curriculum_picture, function(i, pic){
+                                                    curriculum_info +=`<a href="studentDetail.html?studentID=${curriculum.id}"><img src="${pic.url}"></a>`;
+                                                    
+                         });
+                        }
+                        
+                         curriculum_info += `</div>`;
+                         curriculum_info += `<div class="caption">${curriculum_name} </div>`;
+                         curriculum_info += `</div>`;
+                         curriculum_info += `</div>`;
+                    
+                         }
+      $('.curriculum').append(curriculum_info);
+                       });
+}
+
+$.get(all_curriculum_records, allCreatingOpportunitiesRecords);
+
+
+
+
+
+
+
+// try out
+function allCorporateVisitsRecords(data)
+{  
+    $(data.records).each(function(index, curriculum)
+                         {
+                           var curriculum_name = curriculum.fields['Name']
+                           var curriculum_picture = curriculum.fields['Picture']
+                           var curriculum_opportunities = curriculum.fields['OpportunitiesType']
+                           var curriculum_corporateVisits = curriculum.fields['CST']
+                          
+                           var curriculum_info = ''
+                           if (curriculum_name && curriculum_corporateVisits ) {
+                                            curriculum_info += `<div class="column">`;
+                                                curriculum_info += `<div class="col-sm-6 col-md-4">`;
+                                                if (curriculum_picture) {
+                                                    curriculum_info += `<div class="thumbnail">`;
+                                                $.each(curriculum_picture, function(i, pic){
+                                                    curriculum_info +=`<a href="studentDetail.html?studentID=${curriculum.id}"><img src="${pic.url}"></a>`;
+                                                    
+                         });
+                        }
+                        
+                         curriculum_info += `</div>`;
+                         curriculum_info += `<div class="caption">${curriculum_name} </div>`;
+                         curriculum_info += `</div>`;
+                         curriculum_info += `</div>`;
+                    
+                         }
+      $('.corporateVisits').append(curriculum_info);
+                       });
+}
+
+$.get(all_curriculum_records, allCorporateVisitsRecords);
+
