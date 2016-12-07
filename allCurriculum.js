5656f848e3eac8d9b0a4b44138fabb64b80599da
Yuridia Larios <yuriydani@gmail.com>
2016-12-07 20:18:58 +0000

    finally was able to create a thumbnail combining logo, pic, and description fields

--- allCurriculum.js
+++ allCurriculum.js
@@ -10,27 +10,31 @@ function allCreatingOpportunitiesRecords(data)
     $(data.records).each(function(index, curriculum)
                          {
                            var curriculum_name = curriculum.fields['Name']
-                           var curriculum_logo = curriculum.fields['LogoPicture']
-                           var curriculum_date = curriculum.fields['Date']
-                           var curriculum_description = curriculum.fields['Description']
                            var curriculum_picture = curriculum.fields['Picture']
                            var curriculum_opportunities = curriculum.fields['OpportunitiesType']
-                           
+                           var curriculum_corporateVisits = curriculum.fields['CST']
+                           var curriculum_logo = curriculum.fields['LogoPicture']
+                           var curriculum_description = curriculum.fields['Description']
                           
                            var curriculum_info = ''
-                           if (curriculum_name && curriculum_opportunities ) {
+                           if (curriculum_name && curriculum_opportunities) {
                                             curriculum_info += `<div class="column">`;
                                                 curriculum_info += `<div class="col-sm-6 col-md-4">`;
-                                                if (curriculum_name) {
+                                                if (curriculum_picture) {
                                                     curriculum_info += `<div class="thumbnail">`;
-                                                $.each(curriculum_logo, function(i, pic){
-                                                    curriculum_info +=`<a href="curriculumDetail.html?studentID=${curriculum.id}"><img src="${pic.url}"></a>`;
+                                                    
+                                                $.each(curriculum_logo, function(i,pic){
+                                                    curriculum_info += `<img src = "${pic.url}"`;
+                                                });
+                                                $.each(curriculum_picture, function(i, pic){
+                                                    curriculum_info +=`<a href="studentDetail.html?studentID=${curriculum.id}"><img src="${pic.url}"></a>`;
                                                     
                          });
+                                                
                         }
                         
                          curriculum_info += `</div>`;
-                         curriculum_info += `<div class="caption">${curriculum_name} </div>`;
+                         curriculum_info += `<div class="caption">${curriculum_description} </div>`;
                          curriculum_info += `</div>`;
                          curriculum_info += `</div>`;
                     
@@ -38,6 +42,7 @@ function allCreatingOpportunitiesRecords(data)
       $('.opportunities').append(curriculum_info);
                        });
 }
+
 
 $.get(all_curriculum_records, allCreatingOpportunitiesRecords);
 
@@ -55,6 +60,8 @@ function allCorporateVisitsRecords(data)
                            var curriculum_picture = curriculum.fields['Picture']
                            var curriculum_opportunities = curriculum.fields['OpportunitiesType']
                            var curriculum_corporateVisits = curriculum.fields['CST']
+                           var curriculum_logo = curriculum.fields['LogoPicture']
+                           var curriculum_description = curriculum.fields['Description']
                           
                            var curriculum_info = ''
                            if (curriculum_name && curriculum_corporateVisits ) {
@@ -62,14 +69,19 @@ function allCorporateVisitsRecords(data)
                                                 curriculum_info += `<div class="col-sm-6 col-md-4">`;
                                                 if (curriculum_picture) {
                                                     curriculum_info += `<div class="thumbnail">`;
+                                                    
+                                                $.each(curriculum_logo, function(i,pic){
+                                                    curriculum_info += `<img src = "${pic.url}"`;
+                                                });
                                                 $.each(curriculum_picture, function(i, pic){
                                                     curriculum_info +=`<a href="studentDetail.html?studentID=${curriculum.id}"><img src="${pic.url}"></a>`;
                                                     
                          });
+                                                
                         }
                         
                          curriculum_info += `</div>`;
-                         curriculum_info += `<div class="caption">${curriculum_name} </div>`;
+                         curriculum_info += `<div class="caption">${curriculum_description} </div>`;
                          curriculum_info += `</div>`;
                          curriculum_info += `</div>`;
                     
